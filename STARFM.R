63efccc9c729324a9c8c07152b3646c3cf0e071f
Fauzan <119923205+Fauzan922@users.noreply.github.com>
2026-02-06 16:47:46 +0700

    Update STARFM.R

--- STARFM.R
+++ STARFM.R
@@ -12,13 +12,11 @@ landsat_base_file <- "LANDSAT_RES_BASE_20240831.tif"
 modis_base_file <- "RESAMPLE_MODIS_RES_BASE_20240831.tif"
 modis_predict_file <- "RESAMPLE_MODIS_RES_PRED_20240730.tif"
 
-# 4. Set a name for the final output file
+# Output name
 
 output_file_name <- paste0("STARFM_fused_band_NDVI.tif")
 
-## 3. LOAD THE IMAGES (No changes needed)
-# ---------------------------------
-print("Loading your 3 images...")
+## Load images
 # raster() because each file is a single-layer image
 # stack() to load the multi-band files
 L8_stack <- stack(landsat_base_file)
@@ -27,11 +25,8 @@ MOD_stack_pred <- stack(modis_predict_file)
 
 landsat_red_band <- 3
 landsat_nir_band <- 4
-
-# (for MODIS: Red = band 1, NIR = band 2)
 modis_red_band <- 1
 modis_nir_band <- 2
-
 
 landsat_red <- L8_stack[[landsat_red_band]]
 landsat_nir <- L8_stack[[landsat_nir_band]]
@@ -50,12 +45,7 @@ writeRaster(modis_ndvi_base, "NDVI_MODIS_BASE_20240831.tif", overwrite = TRUE)
 modis_ndvi_pred <- (modis_nir_pred - modis_red_pred) / (modis_nir_pred + modis_red_pred)
 writeRaster(modis_ndvi_pred, "NDVI_MODIS_PRED_20240730.tif", overwrite = TRUE)
 
-
-## 4. RUN STARFM
-# ---------------------------------
-print("Starting STARFM fusion... This can take a while.")
-
-# Run the algorithm
+#STARFM
 starfm_job(
     input_filenames = c("NDVI_LANDSAT_20240831.tif", "NDVI_MODIS_BASE_20240831.tif", "NDVI_MODIS_PRED_20240730.tif"),
     input_resolutions = c("high", "low", "low"),
@@ -63,11 +53,8 @@ starfm_job(
     pred_dates = c(2),               # (Predict)
     pred_filenames = c(output_file_name),
 )
-
 print("Fusion complete!")
 
-
-## 5. SAVE THE RESULT
 STARFM_Result <- raster(output_file_name)
 
 print(paste("Success! Fused image saved as:", output_file_name))
@@ -80,9 +67,7 @@ landsat_ndvi_pred <- (landsat_pred_nir - landsat_pred_red) / (landsat_pred_nir +
 plot(landsat_ndvi_pred)
 writeRaster(landsat_ndvi_pred, "NDVI_LANDSAT_20240730.tif", overwrite = TRUE)
 
-## Regresi
-# --- 3. ALIGN AND MASK ---
-# Ensure the images are aligned and have same extent/resolution
+# Regresi
 fused_resampled <- resample(STARFM_Result, landsat_ndvi_pred, method = "bilinear")
 
 # Mask out NA values in either raster
@@ -98,7 +83,7 @@ valid_idx <- which(!is.na(fused_vals) & !is.na(landsat_vals))
 fused_vals <- fused_vals[valid_idx]
 landsat_vals <- landsat_vals[valid_idx]
 
-# --- 4. LINEAR REGRESSION ---
+# Regresi
 regression <- lm(landsat_vals ~ fused_vals)
 summary(regression)
 
